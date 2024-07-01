3585976b68d569a65f9664d87f451af1c28cbaa1
keyisthepast <108239283+PolySkills@users.noreply.github.com>
2024-07-01 18:41:37 -0400

    door.lua

--- door.lua
+++ door.lua
@@ -1,3 +1,5 @@
+-- locks and unlocks door to annoy people in general
+
 while wait() do
 workspace.("House1"):WaitForChild.("Door"):WaitForChild.("ClickDetector")WaitForChild.("RemoteEvent"):FireServer()
 end
