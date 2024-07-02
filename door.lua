d5ff1e512032a23b1de873ee10f4f0404b1dd664
keyisthepast <108239283+PolySkills@users.noreply.github.com>
2024-07-02 14:33:31 -0400

    door.lua

--- door.lua
+++ door.lua
@@ -5,3 +5,8 @@ while wait() do
 workspace.("House1"):WaitForChild.("Door"):WaitForChild.("ClickDetector")WaitForChild.("RemoteEvent"):FireServer()
 end
 
+
+-- second verzion if u wanna mention doors , instead of "House1"
+while wait() do
+workspace.("Door"):WaitForChild.("Lock"):WaitForChild.("ClickDetector")WaitForChild.("RemoteEvent")
+end
